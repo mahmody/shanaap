c2011c4704a6a33facfbfd0459bd80f29c121a32
mahmody <42693632+mahmody@users.noreply.github.com>
2018-12-28 00:44:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ const sql = require("sqlite");
 
 client.on("message", message => {
 	var prefix = "-";
- if (message.content === "-help**)(") {
+ if (message.content === "help)(") {
   const embed = new Discord.RichEmbed() 
       .setColor("#000000") 
       .setDescription(`
